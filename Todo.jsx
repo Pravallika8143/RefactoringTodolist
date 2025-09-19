0f1780c5202fbd4868a42674180abe8c90ab766f
Pravallika8143 <mamidisettipravallika4@gmail.com>
2025-09-19 13:35:28 +0530

    todolist

--- Todo.jsx
+++ Todo.jsx
@@ -5,8 +5,8 @@ function Todo(props){
        <div style={{listStyle:"none"}}>
         <li className="border border-secondary border-3 p-1 m-1">
             <span style={props.t.status?{textDecoration:'line-through'}:{}}>{props.t.title}</span>
-            <button className={props.t.status?"btn btn-success":"btn btn-info"} onClick={()=>{props.done(props.t.id)}}>{props.t.status?"Undo":"Done"}</button>
-            <button className=" m-1 btn btn-danger " onClick={()=>{props.del(props.t.id)}}>Delete</button>
+            <button className={props.t.status?"btn btn-success ms-3":"btn btn-info ms-3"} onClick={()=>{props.done(props.t.id)}}>{props.t.status?"Undo":"Done"}</button>
+            <button className="ms-2 m-1 btn btn-danger " onClick={()=>{props.del(props.t.id)}}>Delete</button>
         </li>
         </div>
     )
