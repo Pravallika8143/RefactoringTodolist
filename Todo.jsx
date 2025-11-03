e850ddc0266db9cc5be32a430315b846b28a0816
Pravallika8143 <mamidisettipravallika4@gmail.com>
2025-11-03 12:20:20 +0530

    todo

--- Todo.jsx
+++ Todo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Todo(props){
     return (
        <div style={{listStyle:"none"}}>
-        <li className="border border-secondary border-3 p-1 m-1">
+        <li className="border border-secondary border-2 p-1 m-1">
             <span style={props.t.status?{textDecoration:'line-through'}:{}}>{props.t.title}</span>
             <button className={props.t.status?"btn btn-success ms-3":"btn btn-info ms-3"} onClick={()=>{props.done(props.t.id)}}>{props.t.status?"Undo":"Done"}</button>
             <button className="ms-2 m-1 btn btn-danger " onClick={()=>{props.del(props.t.id)}}>Delete</button>
