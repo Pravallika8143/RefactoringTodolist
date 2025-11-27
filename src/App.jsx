4615ab77812b3eb929b92ea0c5d41f0e7b9e993d
Pravallika8143 <mamidisettipravallika4@gmail.com>
2025-11-27 12:21:52 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import TodoList from "../Todolist"
 function App(){
 return (
     <>
-    <div className="border border-dark border-3 p-2 m-2">
-      <h1>TodoList Component</h1>
+    <div className="border border-dark border-3 p-2 m-2 text-center">
       <TodoList></TodoList>
     </div>
     </>
